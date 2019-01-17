b9683c983364797e7e39524736fd15b0c6c56999
Etienne Girard <etienne.g.girard@gmail.com>
2019-01-17 13:31:41 +0100

    Clean up code
    - Removed public modifier for JUnit 5 test methods
    - Removed unnecessary endIndex for substring

--- src/main/java/mars/rover/MarsRover.java
+++ src/main/java/mars/rover/MarsRover.java
@@ -7,33 +7,33 @@ public class MarsRover {
             char instruction = instructions.charAt(0);
             if (instruction == 'L') {
                 if (direction == 'N') {
-                    return move(x, y, 'W', instructions.substring(1, instructions.length()));
+                    return move(x, y, 'W', instructions.substring(1));
                 } else if (direction == 'W') {
-                    return move(x, y, 'S', instructions.substring(1, instructions.length()));
+                    return move(x, y, 'S', instructions.substring(1));
                 } else if (direction == 'S') {
-                    return move(x, y, 'E', instructions.substring(1, instructions.length()));
+                    return move(x, y, 'E', instructions.substring(1));
                 } else if (direction == 'E') {
-                    return move(x, y, 'N', instructions.substring(1, instructions.length()));
+                    return move(x, y, 'N', instructions.substring(1));
                 }
             } else if (instruction == 'R') {
                 if (direction == 'N') {
-                    return move(x, y, 'E', instructions.substring(1, instructions.length()));
+                    return move(x, y, 'E', instructions.substring(1));
                 } else if (direction == 'W') {
-                    return move(x, y, 'N', instructions.substring(1, instructions.length()));
+                    return move(x, y, 'N', instructions.substring(1));
                 } else if (direction == 'S') {
-                    return move(x, y, 'W', instructions.substring(1, instructions.length()));
+                    return move(x, y, 'W', instructions.substring(1));
                 } else if (direction == 'E') {
-                    return move(x, y, 'S', instructions.substring(1, instructions.length()));
+                    return move(x, y, 'S', instructions.substring(1));
                 }
             } else if (instruction == 'M') {
                 if (direction == 'N') {
-                    return move(x, y + 1, 'N', instructions.substring(1, instructions.length()));
+                    return move(x, y + 1, 'N', instructions.substring(1));
                 } else if (direction == 'S') {
-                    return move(x, y - 1, 'S', instructions.substring(1, instructions.length()));
+                    return move(x, y - 1, 'S', instructions.substring(1));
                 } else if (direction == 'W') {
-                    return move(x - 1, y, 'W', instructions.substring(1, instructions.length()));
+                    return move(x - 1, y, 'W', instructions.substring(1));
                 } else if (direction == 'E') {
-                    return move(x + 1, y, 'E', instructions.substring(1, instructions.length()));
+                    return move(x + 1, y, 'E', instructions.substring(1));
                 }
             }
         }
