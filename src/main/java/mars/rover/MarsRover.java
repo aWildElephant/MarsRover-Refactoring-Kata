682965533ab4d7718e05c478100eb155795aaa5d
Etienne Girard <etienne.g.girard@gmail.com>
2019-01-20 21:06:06 +0100

    Abstract program input using Reader

--- src/main/java/mars/rover/MarsRover.java
+++ src/main/java/mars/rover/MarsRover.java
@@ -1,39 +1,50 @@
 package mars.rover;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
 public class MarsRover {
 
-    public static String move(int x, int y, char direction, String instructions) {
-        if (!instructions.isEmpty()) {
-            char instruction = instructions.charAt(0);
+    private static final int END_OF_PROGRAM = -1;
+
+    public static String move(int x, int y, char direction, String instructions) throws IOException {
+        return move(x, y, direction, new StringReader(instructions));
+    }
+
+    public static String move(int x, int y, char direction, Reader instructions) throws IOException {
+        final int instruction = instructions.read();
+
+        if (instruction != END_OF_PROGRAM) {
             if (instruction == 'L') {
                 if (direction == 'N') {
-                    return move(x, y, 'W', instructions.substring(1));
+                    return move(x, y, 'W', instructions);
                 } else if (direction == 'W') {
-                    return move(x, y, 'S', instructions.substring(1));
+                    return move(x, y, 'S', instructions);
                 } else if (direction == 'S') {
-                    return move(x, y, 'E', instructions.substring(1));
+                    return move(x, y, 'E', instructions);
                 } else if (direction == 'E') {
-                    return move(x, y, 'N', instructions.substring(1));
+                    return move(x, y, 'N', instructions);
                 }
             } else if (instruction == 'R') {
                 if (direction == 'N') {
-                    return move(x, y, 'E', instructions.substring(1));
+                    return move(x, y, 'E', instructions);
                 } else if (direction == 'W') {
-                    return move(x, y, 'N', instructions.substring(1));
+                    return move(x, y, 'N', instructions);
                 } else if (direction == 'S') {
-                    return move(x, y, 'W', instructions.substring(1));
+                    return move(x, y, 'W', instructions);
                 } else if (direction == 'E') {
-                    return move(x, y, 'S', instructions.substring(1));
+                    return move(x, y, 'S', instructions);
                 }
             } else if (instruction == 'M') {
                 if (direction == 'N') {
-                    return move(x, y + 1, 'N', instructions.substring(1));
+                    return move(x, y + 1, 'N', instructions);
                 } else if (direction == 'S') {
-                    return move(x, y - 1, 'S', instructions.substring(1));
+                    return move(x, y - 1, 'S', instructions);
                 } else if (direction == 'W') {
-                    return move(x - 1, y, 'W', instructions.substring(1));
+                    return move(x - 1, y, 'W', instructions);
                 } else if (direction == 'E') {
-                    return move(x + 1, y, 'E', instructions.substring(1));
+                    return move(x + 1, y, 'E', instructions);
                 }
             }
         }
--- src/test/java/mars/rover/MarsRoverTest.java
+++ src/test/java/mars/rover/MarsRoverTest.java
@@ -2,18 +2,20 @@ package mars.rover;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MarsRoverTest {
 
     @Test
-    void acceptance_test_1() {
+    void acceptance_test_1() throws IOException {
         String newPosition = MarsRover.move(1, 2, 'N', "LMLMLMLMM");
         assertEquals("1 3 N", newPosition);
     }
 
     @Test
-    void acceptance_test_2() {
+    void acceptance_test_2() throws IOException {
         String newPosition = MarsRover.move(3, 3, 'E', "MMRMMRMRRM");
         assertEquals("5 1 E", newPosition);
     }
